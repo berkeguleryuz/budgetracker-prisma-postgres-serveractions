6b99b91f8fabbc3de390c3fe77e98c0e52b613b0
Berke Güleryüz <berkeguleryuz@Berke-MacBook-Pro.local>
2024-05-17 18:14:29 +0300

    update

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,5 @@
-import { PiggyBank } from "lucide-react";
+
+import { TvIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -8,7 +9,7 @@ function Logo() {
   return (
     <div>
       <Link href={"/"} className="flex items-center gap-2">
-        <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
+        <TvIcon className="stroke h-11 w-11 stroke-blue-500 stroke-[1.5]" />
         <p className="bg-gradient-to-r from-blue-400 to-aqua-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
         OmegayonBudget
         </p>
@@ -21,7 +22,7 @@ export function LogoMobile() {
   return (
     <div>
       <Link href={"/"} className="flex items-center gap-2">
-        <p className="bg-gradient-to-r from-blue-400 to-aqua-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        <p className="bg-gradient-to-r from-blue-400 to-blue-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
         OmegayonBudget
         </p>
       </Link>
